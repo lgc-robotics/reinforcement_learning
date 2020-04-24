905128d813babc95a376d50d67a0851d7957475c
LGC <guichaolin@126.com>
2020-04-24 09:03:59 +0800

    Update A3C-MonteCarlo-discrete.py

--- A3C/A3C-MonteCarlo-discrete.py
+++ A3C/A3C-MonteCarlo-discrete.py
@@ -1,3 +1,25 @@
+"""
+Algorithm:
+----------
+A3C algorithm.
+
+Reference:
+----------
+Tensorlayer: https://github.com/tensorlayer/tensorlayer/blob/master/examples/reinforcement_learning/tutorial_AC.py
+MorvanZhou's tutorial: https://morvanzhou.github.io/tutorials/
+
+Environment:
+------------
+CartPole-v0
+CartPole-v1
+#MountainCar-v0
+
+Prerequisites:
+--------------
+tensorflow 2
+gym
+
+"""
 import tensorflow as tf
 import tensorflow_probability as tfp
 from tensorflow.keras.layers import Dense,Input
